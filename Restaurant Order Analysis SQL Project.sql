d45f2f7c36b5b166db49b3c64b616ee350baff38
Anbarasan A <anbarasana1094@gmail.com>
2024-11-17 22:59:36 +0530

    Add files via upload

---
+++ Restaurant Order Analysis SQL Project.sql
@@ -0,0 +1,157 @@
+/* Restaurant Order Analysis SQL Project */
+
+/* Objective 1
+Explore the items table */
+
+/* 1. View the menu_items table and write a query to find the number of items on the menu*/
+
+select distinct count(menu_item_id) as total_no_of_menu 
+from menu_items;
+
+/* 2.  what are the least and most expensive items on the menu? */
+
+select item_name, min(price) as least_expensive_item
+from menu_items
+group by item_name 
+order by least_expensive_item asc
+limit 1 ;
+
+select item_name, max(price) as most_expensive_item
+from menu_items
+group by item_name 
+order by most_expensive_item  desc
+limit 1 ;
+
+/* 3. How many Italian dishes are on the menu?
+ What are the least and most expensive Italian dishes on the menu? */
+ 
+ select distinct count(item_name) as total_italian_dishes
+ from menu_items
+ where category = "Italian";
+ 
+ select item_name, min(price) as least_expensive_itallian_item
+from menu_items
+where category = "Italian"
+group by item_name 
+order by least_expensive_itallian_item asc
+limit 1 ;
+ 
+ 
+ select item_name, max(price) as most_expensive_itallian_item
+from menu_items
+where category = "Italian"
+group by item_name 
+order by most_expensive_itallian_item desc
+limit 1 ;
+
+ /* 4. How many dishes are in each category? 
+ What is the average dish price within each category? */
+ 
+ select category, count(item_name) as total_no_of_dishes_in_category
+ from menu_items
+ group by category;
+ 
+ select category, avg(price) as averge_disheprice_in_category
+ from menu_items
+ group by category;
+ 
+/* Objective 2
+Explore the orders table */
+
+/* 5. View the order_details table. What is the date range of the table? */
+
+select  min(order_date) ,  max(order_date) 
+from  order_details;
+
+/* 6. How many orders were made within this date range? How many items were ordered within this date range?*/
+
+select count(order_id) as no_of_orders
+from order_details
+where order_date between "2023-01-01" And "2023-03-31";
+
+select count( distinct order_id) as no_of_items
+from order_details
+where order_date between "2023-01-01" And "2023-03-31";
+
+ 
+
+/* 7. Which orders had the most number of items? */
+
+/*select order_id, count(item_id) 
+from order_details
+ where item_id = */
+ 
+ select  order_id, count(item_id) 
+from order_details
+group by order_id
+Having count(item_id) = 14;
+
+/* 8. How many orders had more than 12 items? */
+ select  order_id, count(item_id) 
+from order_details
+group by order_id
+Having count(item_id) > 12
+order by count(item_id) desc;
+
+/*Objective 3
+Analyze customer behavior*/
+
+/* 9. Combine the menu_items and order_details tables into a single table */
+
+select *
+from order_details o
+left join menu_items m 
+on o.item_id = m.menu_item_id;
+
+/*What were the least and most ordered items? What categories were they in ?*/
+
+select m.item_name , m.category, count(o.order_details_id) as most_order_item_category
+from order_details o
+left join menu_items m 
+on m.menu_item_id = o.item_id
+group by 1 , 2
+order by most_order_item_category desc
+limit 1;
+
+select m.item_name , m.category, count(o.order_details_id) as least_order_item_category
+from order_details o
+left join menu_items m 
+on m.menu_item_id = o.item_id
+group by 1 , 2
+order by least_order_item_category asc
+limit 1;
+
+/*What were the top 5 orders that spent the most money?*/
+
+select o.order_id , sum(price) as total_price
+from order_details o
+left join menu_items m 
+on m.menu_item_id = o.item_id
+group by 1 
+order by total_price desc
+limit 5;
+
+/*View the details of the highest spend order. Which specific items were purchased?*/
+
+select m.category , count(item_id)  as total_purchased_item
+from order_details o
+left join menu_items m 
+on m.menu_item_id = o.item_id
+where o.order_id = 440
+group by 1
+
+/* View the details of the top 5 highest spend orders */
+
+select o.order_id , count(o.item_id), sum(price) as total_price
+from order_details o
+left join menu_items m 
+on m.menu_item_id = o.item_id
+group by 1 
+order by total_price desc
+limit 5;
+
+
+
+
+
+
